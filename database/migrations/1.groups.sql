826f78c71c97215142ff61cc115cbb2292f2938c
jamesread <contact@jread.com>
2025-01-06 23:54:38 +0000

    SQL Syntax error

--- database/migrations/1.groups.sql
+++ database/migrations/1.groups.sql
@@ -1,6 +1,6 @@
 -- +migrate Up
-INSERT INTO groups (id, title) VALUES (1, 'Admins');
-INSERT INTO groups (id, title) VALUES (2, 'Users');
+INSERT INTO `groups` (id, title) VALUES (1, 'Admins');
+INSERT INTO `groups` (id, title) VALUES (2, 'Users');
 INSERT INTO permissions (id, `key`) VALUES (1, 'SUPERUSER');
 INSERT INTO privileges_g (`group`, `permission`) VALUES (1, 1);
 
