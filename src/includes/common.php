1a6b76b06e721244f15bf01d4bc8bd5dd9961060
jamesread <contact@jread.com>
2025-01-06 01:00:03 +0000

    Startup code

--- src/includes/common.php
+++ src/includes/common.php
@@ -31,51 +31,7 @@ use libAllure\DatabaseFactory;
 $db = new Database($cfg->getDsn(), $cfg->get('DB_USER'), $cfg->get('DB_PASS'));
 DatabaseFactory::registerInstance($db);
 
-function startupError($message)
-{
-    $message = nl2br($message);
-    $message = <<<HTML
-<head>
-<title>Faridoon startup error</title>
-<style type = "text/css">
-body {
-    background-color: #efefef;
-    font-family: sans-serif;
-    padding: 2em;
-}
-</style>
-</head>
-<body>
-    <h1>Faridoon startup error</h1>
-    $message
-</body>
-HTML;
-    echo $message;
-
-    exit;
-}
-
-function requireDatabaseVersion(string $requiredMigration)
-{
-    try {
-        $sql = 'SELECT id FROM migrations';
-        $stmt = libAllure\DatabaseFactory::getInstance()->query($sql);
-        $versionRows = array_column($stmt->fetchAll(), 'id');
-    } catch (Exception $e) {
-        startupError('Faridoon connected to the database, but the migrations table could not be queried.');
-    }
-
-    natsort($versionRows);
-    $latestVersion = end($versionRows);
-
-    if ($latestVersion != $requiredMigration) {
-        if ($latestVersion == '') {
-            $latestVersion = 'null';
-        }
-
-        startupError('Faridoon requires database version ' . $requiredMigration . ' but the database is at version ' . $latestVersion . '. Please <a href = "http://jamesread.github.io/Faridoon/installation/migrations/">run database migrations</a>.');
-    }
-}
+require_once 'includes/startup.php';
 
 requireDatabaseVersion('0.base.sql');
 
